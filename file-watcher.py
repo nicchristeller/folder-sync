95e954c88adf7d9c64292c66266810d3bd59f4ca
Nic Christeller <ncc46@uclive.ac.nz>
2016-10-10 16:55:28 +1300

    Started creating change observers rather than complete directory traversal looking for changes

---
+++ file-watcher.py
@@ -0,0 +1,50 @@
+import logging
+import sys
+import time
+
+from watchdog.events import FileSystemEventHandler
+
+from watchdog.observers import Observer
+
+
+class MyEventHandler(FileSystemEventHandler):
+
+    def __init__(self):
+        self.recent_events = []
+
+    def on_any_event(self, event):
+        self.recent_events.append(event)
+
+    def on_moved(self, event):
+        what = 'directory' if event.is_directory else 'file'
+        logging.info("Moved %s: from %s to %s", what, event.src_path,
+                     event.dest_path)
+
+    def on_created(self, event):
+        what = 'directory' if event.is_directory else 'file'
+        logging.info("Created %s: %s", what, event.src_path)
+
+    def on_deleted(self, event):
+        what = 'directory' if event.is_directory else 'file'
+        logging.info("Deleted %s: %s", what, event.src_path)
+
+    def on_modified(self, event):
+        what = 'directory' if event.is_directory else 'file'
+        logging.info("Modified %s: %s", what, event.src_path)
+
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO,
+                        format='%(asctime)s - %(message)s',
+                        datefmt='%Y-%m-%d %H:%M:%S')
+    path = sys.argv[1] if len(sys.argv) > 1 else "C:\\Users\\Nic\\Documents\\Mine"
+    event_handler = MyEventHandler()
+    observer = Observer()
+    observer.schedule(event_handler, path, recursive=True)
+    observer.start()
+    try:
+        while True:
+            time.sleep(1)
+    except KeyboardInterrupt:
+        observer.stop()
+    observer.join()
